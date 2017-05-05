2c2e4ac0186a86e7954a0fb2076edb907418ac51
hcsadm <hcsadm-junkmail@yahoo.com>
2017-05-05 13:52:10 -0700

    Added Vendor metadata annotations

---
+++ AdventureWorksSolution/ISIncClassProject/Models/VendorMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ISIncClassProject.Entities
+{
+    public class VendorMetadata
+    {
+
+        [Display(Name = "Account Number")]
+        public string AccountNumber;
+
+        [Display(Name = "Credit Rating")]
+        public byte CreditRating;
+
+        [Display(Name = "Active")]
+        public bool ActiveFlag;
+
+        [Display(Name = "Preferred")]
+        public bool PreferredVendorStatus;
+
+        [Display(Name = "Modified")]
+        public System.DateTime ModifiedDate;
+    }
+}
